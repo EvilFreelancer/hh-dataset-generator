7e90692d1cc15c9140787a65ad3e5a56b0cb5980
Paul Rock <paul@drteam.rocks>
2022-11-11 02:48:45 +0300

    Deep tunes of filtration

--- generate.py
+++ generate.py
@@ -1,13 +1,19 @@
+# -*- coding: utf-8 -*-
+
 import json
 import os
 import re
 import html
 import csv
+import glob
 from datetime import datetime
 
 # Datetime object containing current date and time
 now = datetime.now()
 
+# Load JSON files
+excludes = json.loads(open('./excludes.json', "r").read())
+aliases = json.loads(open('./aliases.json', "r").read())
 
 # https://stackoverflow.com/questions/753052/strip-html-from-strings-in-python
 def strip_tags(value):
@@ -26,7 +32,7 @@ filewriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE
 filewriter.writerow(['text', 'keys'])
 
 # Get list of all scrapped vacancies
-vacancies = os.listdir('./docs/vacancies')
+vacancies = glob.glob('./docs/vacancies/*')
 vacanciesCount = len(vacancies)
 i = 0
 for fl in vacancies:
@@ -34,7 +40,7 @@ for fl in vacancies:
     i = i + 1
 
     # Open file
-    f = open('./docs/vacancies/{}'.format(fl), encoding='utf8')
+    f = open(fl, encoding='utf8')
     jsonText = f.read()
     f.close()
 
@@ -48,12 +54,55 @@ for fl in vacancies:
     # Extract only required fields
     id = jsonArr['id']
     name = jsonArr['name']
-    description = strip_tags(jsonArr['description'])
-    key_skills = ",".join([key_skill.get('name') for key_skill in jsonArr['key_skills']])
+    description = strip_tags(jsonArr['description']).lower()
+    key_skills = [skill.get('name') for skill in jsonArr['key_skills']]
+
+    # Damn... Pandas, Numpy, Matplotlib / Plotly
+    tmpKeySkills = []
+    for skill in key_skills:
+        if ',' in skill:
+            tmpKeySkills + skill.split(',')
+        else:
+            tmpKeySkills.append(skill)
+
+    # Sanitize list
+    filterKeySkills = []
+    for skill in tmpKeySkills:
+        _skill = skill.lower().strip().rstrip('.')
+        # Let's skip excludes
+        if _skill in excludes:
+            continue
+
+        # Check if skill is not an alias
+        filterSkill = _skill
+        for key, value in aliases.items():
+            if _skill in value:
+                filterSkill = key.lower()
+
+        # Add to output array
+        filterKeySkills.append(filterSkill)
+
+    # Sort and remove duplicates
+    filterKeySkills = sorted(set(filterKeySkills))
+
+    # Parse skill, we need keys which exists in text
+    skills = []
+    for skill in filterKeySkills:
+        # Check if skill from keys is in main text
+        if re.search(r'' + re.escape(skill), name + ' ' + description, re.MULTILINE | re.IGNORECASE):
+            skills.append(skill)
+
+    # If key_skills list is empty, then skip this step
+    if not skills:
+        continue
+
+    # Build string of skills
+    skills = sorted(skills)
+    skills = ",".join(skills)
 
     # Status report
-    message = '[{}/{}] id:{} name:{}'.format(i, vacanciesCount, id, name)
+    message = '[{}/{}] id:{} skills:{}'.format(i, vacanciesCount, id, skills)
     print(message)
 
     # Put data to file
-    filewriter.writerow([name + " " + description, key_skills])
+    filewriter.writerow([name + " " + description, skills])
